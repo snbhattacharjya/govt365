d37734d611c938c2b265172b235fb32c646b15b5
Saikat Narayan Bhattacharjya <snbhattacharjya@gmail.com>
2017-08-21 17:18:36 +0530

    Changes made for Frontend and Application Model

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -46,12 +46,16 @@
                             <li><a href="{{ route('login') }}">Login</a></li>
                             <li><a href="{{ route('register') }}">Register</a></li>
                         @else
+                            <li><a href="{{ route('applications.show') }}">Application</a></li>
                             <li class="dropdown">
                                 <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
                                     {{ Auth::user()->name }} <span class="caret"></span>
                                 </a>
 
                                 <ul class="dropdown-menu" role="menu">
+                                    <li><a href="#">Profile</a></li>
+                                    <li><a href="#">Change Password</a></li>
+                                    <li role="separator" class="divider"></li>
                                     <li>
                                         <a href="{{ route('logout') }}"
                                             onclick="event.preventDefault();
