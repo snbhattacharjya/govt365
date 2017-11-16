ec616897df9d641d21c14facf83ffe2b6142d821
Saikat Narayan Bhattacharjya <snbhattacharjya@gmail.com>
2017-11-16 17:37:25 +0530

    Completing Application CRUD

--- resources/views/application/index.blade.php
+++ resources/views/application/index.blade.php
@@ -2,8 +2,9 @@
 
 @section('content')
 <div class="container">
+  @include('layouts.message')
   <div class="row">
-    <div class="col-md-8">
+    <div class="col-md-10">
       <div class="page-header">
         <h1>Applications <small>Home</small></h1>
       </div>
@@ -14,6 +15,8 @@
             <th>Applicant Name</th>
             <th>Brief History</th>
             <th>Receive Date</th>
+            <th>Create At</th>
+            <th>Modified At</th>
             <th></th>
           </tr>
         </thead>
@@ -23,14 +26,16 @@
               <td>{{ $application->id }}</td>
               <td>{{ $application->appl_name }}</td>
               <td>{{ $application->brief_history }}</td>
-              <td>{{ $application->receive_date }}</td>
+              <td>{{ date('M, d Y (D)',strtotime($application->receive_date)) }}</td>
+              <td>{{ date('M, d Y H:i:s',strtotime($application->created_at)) }}</td>
+              <td>{{ date('M, d Y H:i:s',strtotime($application->updated_at)) }}</td>
               <td><a href="{{ route('application.show',$application->id) }}" class="btn btn-info btn-sm">Read More</a></td>
             </tr>
           @endforeach
         </tbody>
       </table>
     </div>
-    <div class="col-md-4">
+    <div class="col-md-2">
       <div class="well well-lg">
           <a href="{{ route('application.create') }}" class="btn btn-success">Create New</a>
       </div>
